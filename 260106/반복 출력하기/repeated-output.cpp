742c3095a3e7d15e07e8ef666fc814788fac7a63
okim7979 <49549602+okim7979@users.noreply.github.com>
2026-01-06 01:46:40 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260106/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void Printa(int a) {
+    for (int i = 0; i < a; i++) {
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    int a;
+    cin >> a;
+    Printa(a);
+    return 0;
+}

f95dc640c2fc3613aa45936fda30012481e3db4c
okim7979 <49549602+okim7979@users.noreply.github.com>
2026-01-30 12:00:33 +0900

    Title: 2줄 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260130/2줄 출력/print-two-lines.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        sb.append("Hello").append("\n").append("World");
+        System.out.println(sb)
+    }
+}

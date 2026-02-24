73d4307d785ca50e88c042f96c0c07230ae44d08
okim7979 <49549602+okim7979@users.noreply.github.com>
2026-02-25 01:35:09 +0900

    Title: 1차원 윷놀이, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260225/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m, k = map(int, input().split())
+nums = list(map(int, input().split()))
+
+num_is_over = [1]*k
+max_num = float('-inf')
+
+def findings():
+    global max_num
+    count = 0
+    for i in num_is_over:
+        if i >= m:
+            count += 1
+    if count > max_num:
+        max_num = count
+
+def choose(curr_num):
+    if curr_num == n:
+        findings()
+        return
+
+    for i in range(k):
+        if num_is_over[i] >= m:
+            continue
+
+        num_is_over[i] += nums[curr_num]
+        choose(curr_num+1)
+
+        num_is_over[i] -= nums[curr_num]
+
+choose(0)
+print(max_num)
